787c029ced8493bcd1e40f5006c345631c5225c1
Matias Spina Mattera <rubiioibz@hotmail.com>
2022-01-14 17:19:19 +0100

    primer commit de pruebas en js

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//LOGIN
+
+let user = "matias";
+let pass = "mattera";
+
+let userName = prompt("Ingrese nombre de Usuario:");
+let userPass = prompt("Ingrese contraseña:");
+
+while((user != userName) || (pass != userPass)){
+
+    if(user === userName && pass === userPass){
+        alert("Bienvenido al Sitio web de Matias Mattera");
+    }
+    else if ((user != userName) && (pass != userPass)){
+        alert("Usuario y contraseña incorrecto");
+    }
+    else if (user != userName){
+        alert("Nombre de usuario incorrecto");
+    }
+    else if (pass != userPass){
+        alert("Contraseña incorrecta");
+    }else{
+        alert("ERROR");
+    };
+
+    userName = prompt("Ingrese nombre de Usuario:");
+    userPass = prompt("Ingrese contraseña:");
+}
